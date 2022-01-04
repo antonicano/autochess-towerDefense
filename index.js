f40df97841404ed07cf453fd9921dee81b707693
Antoni Cano <antoni.cano96@gmai.com>
2022-01-04 18:48:32 +0100

    full dynamic starter interface

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const ROWS = 5;
 const CELL_PER_ROW = 5;
+const HEROES =[
+    {
+        "name": "megaman",
+        "url": "megaman.png"
+    },
+    {
+        "name": "sonic",
+        "url" : "sonic.png"
+    }
+]
 
 $( document ).ready(function() {
     console.log('ready');
     setGameTable();
     setDropZones();
+    loadHeroes();
   });
 
   function allowDrop(ev) {
@@ -25,11 +36,9 @@ function setGameTable(){
     const gameTable = document.getElementById('gameTable');
 
     for(let i = 0; i < ROWS; i++){
-        console.log('row');
         let tableRow = document.createElement("div");
         tableRow.classList.add('game_row');
         for(let j = 0; j < CELL_PER_ROW; j++){
-            console.log('cell');
             let gameCell = document.createElement("div");
             gameCell.classList.add('game_cell');
             tableRow.appendChild(gameCell);
@@ -54,4 +63,28 @@ function setDropZones(){
 
 function setDragZones(){
 
+}
+
+function loadHeroes(){
+    const gameDock = document.getElementById('gameDock');
+    for(let i = 0; i < HEROES.length; i++){
+        const heroHolder = document.createElement('div');
+        heroHolder.classList.add('hero_holder');
+        const heroImg = document.createElement('img');
+        heroImg.classList.add('hero_img');
+        heroImg.id = HEROES[i].name;
+        heroImg.src = "images/"+HEROES[i].url;
+
+        heroHolder.appendChild(heroImg);
+        gameDock.appendChild(heroHolder);
+        
+        console.log(HEROES[i]);
+    }
+
+    const heroImgs = document.getElementsByClassName('hero_img');
+    for(let i = 0; i < heroImgs.length; i++){
+        heroImgs[i].addEventListener("dragstart", function(event){
+            drag(event);
+        });
+    }
 }
