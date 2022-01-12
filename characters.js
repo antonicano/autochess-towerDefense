5d46ec7e3425cd05d65ec3e66da14a0014ad7912
Antoni Cano <antoni.cano96@gmai.com>
2022-01-12 20:38:25 +0100

    borde en hero holders, personajes en fchero separado, turno de combate

---
+++ characters.js
@@ -0,0 +1,58 @@
+const HEROES =[
+    {
+        "name": "angemon",
+        "url": "angemon.png",
+        "range": 5,
+        "attack": 4,
+        "defense": 4
+    },
+    {
+        "name": "digimon",
+        "url" : "digimon.png",
+        "range" : 2,
+        "attack": 10,
+        "defense" :3
+    },
+    {
+        "name": "guts",
+        "url" : "guts.png",
+        "range" : 2,
+        "attack": 10,
+        "defense" :3
+    }
+];
+
+const ENEMIES = [
+    {
+        "name": "archangel",
+        "url": "archangel.png"
+    },
+    {
+        "name": "diablo",
+        "url" : "diablo.png"
+    },
+    {
+        "name": "dt",
+        "url" : "dt.png"
+    },
+    {
+        "name": "illidan",
+        "url" : "illidan.png"
+    },
+    {
+        "name": "kerrigan",
+        "url" : "kerrigan.png"
+    },
+    {
+        "name": "lich",
+        "url" : "lich.png"
+    },
+    {
+        "name": "monkey",
+        "url" : "monkey.png"
+    },
+    {
+        "name": "reaper",
+        "url" : "reaper.png"
+    },
+];
